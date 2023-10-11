c449f18f3a125b04291fe2151edc65a6a5b62055
Pabloandres4112 <perdomo4112@gmail.com>
2023-10-11 16:36:55 -0500

    carito

--- js/carrito.js
+++ js/carrito.js
@@ -1,20 +1,32 @@
-let boxMinus = document.getElementById("boxminus");
-let boxCantidad = document.getElementById("boxcan");
-let boxPlus = document.getElementById("boxplus");
+let boxMinus = document.getElementById("boxminus"),
+boxCantidad = document.getElementById("boxcan"),
+boxPlus = document.getElementById("boxplus"),
+valorProducto = 260000,
+Cantidad = document.getElementById("numeroPRoductos"),
+auxValorproducto = new Intl.NumberFormat("de-DE").format(valorProducto);
+document.getElementById("precioUnidad").innerHTML= auxValorproducto;
+
 
 boxPlus.addEventListener("click",function(){
-    
     let axucant = boxCantidad.innerHTML;
     if(axucant < 10){
         axucant++;
         boxCantidad.innerHTML = axucant;
+        Cantidad.innerHTML=axucant;
+        multipli = valorProducto *axucant
+        let auxsubtotal = new  Intl.NumberFormat("de-DE").format(multipli);
+        subtotal.innerHTML = auxsubtotal;
     }
 
 });
 boxMinus.addEventListener("click",function(){
     let resta = boxCantidad.innerHTML;
-    if (resta >0);{
+    if (resta > 0){
         resta --;
         boxCantidad.innerHTML = resta;
     }
+    Cantidad.innerHTML = resta
+    resta = valorProducto *resta 
+    let auxsubtotal = new  Intl.NumberFormat("de-DE").format(resta);
+    subtotal.innerHTML = auxsubtotal;
 });
