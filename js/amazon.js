b57acfcc04369dc903ba67ca408181c5e25dbc75
pabloandres4112 <perdomo4112@gmail.com>
2023-11-22 16:33:34 -0500

    amason

--- js/amazon.js
+++ js/amazon.js
@@ -19,3 +19,43 @@ imagenes.forEach((imagen) => {
 btnClose.addEventListener('click', function(){
     mainContent.style.display = "none";
     });
+
+//////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+
+    var ratingGroup = document.getElementById('half-stars-example');
+    var ratingInputs = ratingGroup.querySelectorAll('input[type="radio"]');
+    var ratingLabels = ratingGroup.querySelectorAll('.rating__label');
+    var result = document.querySelector("#current-rating")
+
+    let selectedRating = 2.5;
+
+    ratingInputs.forEach((input) => {
+        input.addEventListener('change', (event) => {
+            selectedRating = parseFloat(event.target.value);
+            updateRatingDisplay();
+        });
+    });
+
+    ratingLabels.forEach((label) => {
+        label.addEventListener('mouseover', (event) => {
+            var labelValue = parseFloat(event.target.getAttribute('for').split('-')[1]);
+            updateRatingDisplay(labelValue);
+        });
+
+        label.addEventListener('mouseout', () => {
+            updateRatingDisplay();
+        });
+    });
+
+    function updateRatingDisplay(ratingValue = selectedRating) {
+        var ratingDisplay = document.getElementById('rating-display');
+        if (!ratingDisplay) {
+            ratingDisplay = document.createElement('p');
+            ratingDisplay.id = 'rating-display';
+            ratingGroup.parentNode.appendChild(ratingDisplay);
+        }
+        result.textContent = ratingValue
+    }
--- js/prueba.js
+++ js/prueba.js
@@ -1,50 +1,34 @@
-let boxMinus = document.getElementById("boxMinus"),
-    boxCant = document.getElementById("boxCant"),
-    boxPlus = document.getElementById("boxPlus"),
-    numeroProductos = document.getElementById("numeroPoductos"),
-    ValorProducto = 50000 ,
-    auxValorProducto = new Intl.NumberFormat("de-DE").format(ValorProducto);
-    document.getElementById("precioUnidad").innerHTML = auxValorProducto;
+var ratingGroup = document.getElementById('half-stars-example');
+var ratingInputs = ratingGroup.querySelectorAll('input[type="radio"]');
+var ratingLabels = ratingGroup.querySelectorAll('.rating__label');
+var result = document.querySelector("#current-rating");
 
+let selectedRating = 2.5;
 
-    
-    boxPlus.addEventListener("click", function(){
-        let PlusCant = boxCantidad.innerHTML;
-        if(PlusCant < 10){
-            PlusCant++;
-            boxCantidad.innerText=PlusCant
-            determinarSingularPlural(PlusCant)
-            //calcular precio total
-    let SubTotal = ValorProducto*PlusCant,
-    auxSubtotal = new Intl.NumberFormat("de-DE").format(SubTotal);
-    document.getElementById("Subtotal").innerHTML = auxSubtotal;
+ratingInputs.forEach((input) => {
+    input.addEventListener('change', (event) => {
+        selectedRating = parseFloat(event.target.value);
+        updateRatingDisplay();
+    });
+});
 
+ratingLabels.forEach((label) => {
+    label.addEventListener('mouseover', (event) => {
+        var labelValue = parseFloat(event.target.getAttribute('for').split('-')[1]);
+        updateRatingDisplay(labelValue);
+    });
 
-        }
-        
-    })
+    label.addEventListener('mouseout', () => {
+        updateRatingDisplay();
+    });
+});
 
-
-    boxMinus.addEventListener("click", function(){
-        let MinusCant = boxCantidad.innerHTML;
-        if(MinusCant > 0){
-            MinusCant--;
-            boxCantidad.innerText=MinusCant 
-            determinarSingularPlural(MinusCant)
-    
-            // calcular precio total
-            let SubTotal = ValorProducto*MinusCant ,
-            auxSubtotal = new Intl.NumberFormat("de-DE").format(SubTotal);
-            document.getElementById("Subtotal").innerHTML = auxSubtotal;
-
-
-        }
-    })
-    function determinarSingularPlural(PlusCant){
-        if (PlusCant == 1){
-            numeroProductos.innerHTML ="( " + PlusCant + " producto)"
-        }
-        else if ((PlusCant == 0 || PlusCant > 1)) {
-            numeroProductos.innerHTML = "( " + PlusCant + " productos)"
-        }
+function updateRatingDisplay(ratingValue = selectedRating) {
+    var ratingDisplay = document.getElementById('rating-display');
+    if (!ratingDisplay) {
+        ratingDisplay = document.createElement('p');
+        ratingDisplay.id = 'rating-display';
+        ratingGroup.parentNode.appendChild(ratingDisplay);
     }
+    result.textContent = ratingValue;
+}
